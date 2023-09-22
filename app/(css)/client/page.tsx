a76a8539e7026b60f63e4d30a844b075cd7be0e1
Geuni <dlrmsgnl620@naver.com>
2023-09-22 22:12:46 +0900

    global css

---
+++ app/(css)/client/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const ClientComponents = () => {
+  return (
+    <div className="fontApply mx-auto flex h-[500px] w-[500px] items-center justify-center text-4xl">
+      <p>여기는 client components 입니다.</p>
+    </div>
+  );
+};
+
+export default ClientComponents;
---
+++ app/(css)/server/page.tsx
@@ -0,0 +1,9 @@
+const ServerComponents = () => {
+  return (
+    <div className="fontApply mx-auto flex h-[500px] w-[500px] items-center justify-center text-4xl">
+      <p>여기는 client components 입니다.</p>
+    </div>
+  );
+};
+
+export default ServerComponents;
